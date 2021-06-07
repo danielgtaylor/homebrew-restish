b6ef7f4841b86f7dc40fd8fa225566c893e80300
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-07 06:59:05 -0700

    Brew formula update for restish version v0.8.0

--- restish.rb
+++ restish.rb
@@ -5,20 +5,25 @@
 class Restish < Formula
   desc "Restish is a CLI for interacting with REST-ish HTTP APIs with some nice features built-in."
   homepage "https://rest.sh/"
-  version "0.7.0"
+  version "0.8.0"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/danielgtaylor/restish/releases/download/v0.7.0/restish-0.7.0-mac-x86_64.tar.gz"
-    sha256 "d37b022d6cc5f07f116aa9de92939d88581967329fcbb4563f5c58c2170c0760"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/danielgtaylor/restish/releases/download/v0.8.0/restish-0.8.0-mac-x86_64.tar.gz"
+      sha256 "f2f4be1174d843aa4d776497746295de30cf83f5b7e5097e4d5cc79bfd4e58de"
+    end
   end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/danielgtaylor/restish/releases/download/v0.7.0/restish-0.7.0-linux-x86_64.tar.gz"
-    sha256 "7f763f82fa620fbcefd2ed6dd265be1bcfb80ac18ad6ee61c6e130fd97624629"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/danielgtaylor/restish/releases/download/v0.7.0/restish-0.7.0-linux-arm64.tar.gz"
-    sha256 "d28de88f4308726d4bcc2603df07ef40730bc1c823cd7df3ab4985517ce6ebe6"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/danielgtaylor/restish/releases/download/v0.8.0/restish-0.8.0-linux-x86_64.tar.gz"
+      sha256 "fe9f8ff2955be223dc0048798041712ba3ca0ab3e21bb499eda2ed313da545b5"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/danielgtaylor/restish/releases/download/v0.8.0/restish-0.8.0-linux-arm64.tar.gz"
+      sha256 "50e71830950ee56a0c41609e79db57a93d861f55621c8ebfd039cdd0794ea9d7"
+    end
   end
 
   def install
